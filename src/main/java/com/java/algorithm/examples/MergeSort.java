36eebd605ea21202abd2375bd930c80dbc7272b0
yikangfeng <yikangfeng@LP-BJ8376.vipkid.work>
2018-09-18 17:40:52 +0800

    add merge sort

--- src/main/java/com/java/algorithm/examples/MergeSort.java
+++ src/main/java/com/java/algorithm/examples/MergeSort.java
@@ -9,11 +9,42 @@ public class MergeSort extends BaseSort {
 	}
 
 	private void mergeSort() {
-		doMergeSort(array);
+		int[] temp = new int[array.length];
+		doMergeSort(array, 0, array.length - 1, temp);
 		printArray();
 	}
 
-	private void doMergeSort(int[] array) {
+	private void doMergeSort(int[] array, int start, int end, int[] temp) {
+
+		if (start == end)
+			return;
+
+		int _start = start;
+		int _end = end;
+		int mid = (_start + _end) / 2;
+
+		doMergeSort(array, _start, mid, temp);
+		doMergeSort(array, mid + 1, _end, temp);
+
+		int tempIndex = 0;
+		int _mid=mid+1;
+		while (_start <= mid && _mid <= _end) {
+			if (array[_start] <= array[_mid]) {
+				temp[tempIndex++] = array[_start++];
+			} else {
+				temp[tempIndex++] = array[_mid++];
+			}
+		}
+
+		while (_start <= mid)
+			temp[tempIndex++] = array[_start++];
+
+		while (_mid <= _end)
+			temp[tempIndex++] = array[_mid++];
+
+		tempIndex = 0;
+		while (start <= end)
+			array[start++] = temp[tempIndex++];
 
 	}
 
