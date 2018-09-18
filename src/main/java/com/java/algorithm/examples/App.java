2108bcafab621ca15ba8c1fb92a23873532d1134
yikangfeng <yikangfeng@LP-BJ8376.vipkid.work>
2018-09-18 12:55:51 +0800

    first commit

---
+++ src/main/java/com/java/algorithm/examples/App.java
@@ -0,0 +1,13 @@
+package com.java.algorithm.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
